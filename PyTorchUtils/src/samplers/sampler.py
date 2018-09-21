e55c37b2db9e53e619363a6c81e9cdddfccd9ce2
zahramansoor <zahradhanerawala@gmail.com>
2018-09-21 16:41:03 -0400

    mods of cshl 3dunet

---
+++ PyTorchUtils/src/samplers/sampler.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+__doc__= """
+
+Sampling class for joint synapse and vesicle training
+
+Nicholas Turner <nturner@cs.princeton.edu>, 2017
+"""
+import os
+
+import dataprovider as dp
+import h5py
+
+def read_file(fname):
+
+    f = h5py.File(fname)
+    d = f["/main"].value
+    f.close()
+
+    return d
+
+
+class Sampler(object):
+
+    def __init__(self, datadir, dsets=[], 
+                                mode="train", patchsz=(10,32,32)): #original: 18 x 160 x 160
+
+      assert mode in ["train","val","test"]
+
+      datadir = os.path.expanduser(datadir)
+  
+      volnames = ["input", "cell_label"]
+      spec = { name : patchsz for name in volnames }
+
+      self.dp = self.build_data_provider(datadir, spec, mode, dsets)
+
+
+    def __call__(self, **kwargs):
+      return self.dp("random", **kwargs)
+
+
+    def build_data_provider(self, datadir, spec, mode, dsets):
+
+      vdp = dp.VolumeDataProvider()
+
+      for dset in dsets:
+        vdp.add_dataset( self.build_dataset(datadir, spec, dset) )
+      
+      vdp.set_sampling_weights()
+
+      vdp.set_augmentor(self._aug(mode))
+      vdp.set_postprocessor(self._post())
+
+      return vdp
+
+
+    def build_dataset(self, datadir, spec, dset_name):
+      
+      img = read_file(os.path.join(os.path.join(datadir, 'inputRawImages'), dset_name + "_inputRawImages.h5")) #+'/inputRawImages' added by zmd 20180917      
+      #print '\n     Read raw file correctly!\n'
+      
+      lbl = read_file(os.path.join(os.path.join(datadir, 'inputLabelImages'), dset_name + "_inputLabelImages.h5")).astype("float32") #+'/inputInputImages' added by zmd 20180917
+      #print '\n     Read label file correctly!\n'
+      
+      img = dp.transform.divideby(img, val=2000.0, dtype="float32")
+      lbl[lbl != 0] = 1
+
+      vd = dp.VolumeDataset()
+      vd.add_raw_data(key="input",       data=img)
+      vd.add_raw_data(key="cell_label",  data=lbl)
+      
+      vd.set_spec(spec)
+      
+      print '\n     Built dataset for {}'.format(dset_name)
+      return vd
+
+
+    def _aug(self, mode):
+
+      aug = dp.Augmentor()
+      if mode == "train":
+        aug.append('misalign', max_trans=17.0)
+      aug.append('missing', max_sec=5, mode='mix',random_color=True)
+      aug.append('blur', max_sec=5, mode='mix')
+      if mode == "train":
+        aug.append('warp')
+        aug.append('greyscale', mode='mix')
+        aug.append('flip')
+      return aug
+
+
+    def _post(self):
+      post = dp.Transformer()
+      return post
+
--- basic_utils/convert.py
+++ basic_utils/convert.py
@@ -3,11 +3,35 @@
 """
 Created on Wed Sep 19 11:28:04 2018
 
-@author: wanglab
+@author: wanglab, tpisano
 """
 import h5py
 from skimage.external import tifffile
+from skimage.io import imread
+import multiprocessing as mp
+import numpy as np
+import os, time, sys, cv2
+from math import ceil
+from os.path import basename, splitext, isfile
+from tools.utils.io import load_kwargs, writer
+from glob import glob
 
+#%%
+def convert_tiff(tiff_filename):
+    '''
+    by James Gornet @ Columbia
+    '''
+    with tifffile.TiffFile(tiff_filename) as tif:
+        if not isfile(tiff_filename):
+            error_string = "{} does not exist".format(basename(tiff_filename))
+            raise EnvironmentError(error_string)
+        
+        h5_filename = splitext(tiff_filename)[0] + ".h5"
+
+        with h5py.File(h5_filename) as h5_file:
+            h5_file.create_dataset("/main", data=tif.asarray())
+            
+            
 def convert_h5_to_tiff(img_pth, img_tif_pth = False):
     '''Function to convert h5 files to tiff stacks. 
     Used for the 3dunet pipeline to check forward pass output.
@@ -28,3 +52,42 @@ def convert_h5_to_tiff(img_pth, img_tif_pth = False):
         tifffile.imsave(img_tif_pth, arr)
     else: 
         tifffile.imsave('/home/wanglab/Desktop/test.tif', arr)
+        
+def fullsizedata_to_h5(dct, dst):
+    '''Function to convert full_sizedatafld tiffs from a whole brain lightsheet image into a single h5 file to feed into 3dunet.
+    Faster than convert tiffs to stack and stack to h5.
+    Inputs:
+        dct = processed data folder containing full_sizedatafld of cell channel
+        dst = destination path of h5 file
+    
+    Returns:
+        hdf5 dataset
+    Inspired by: https://stackoverflow.com/questions/31951507/out-of-core-4d-image-tif-storage-as-hdf5-python
+    '''
+    start = time.time()
+    kwargs = load_kwargs(dct)
+    #define cell channel volume
+    vol = [xx for xx in kwargs['volumes'] if xx.ch_type == 'cellch'][0]
+    #define full_sizedatafld subfolder containing cell channel tifs
+    cellch_vol = vol.full_sizedatafld_vol
+    zplns = sorted(glob(os.path.join(cellch_vol, vol.brainname + '_ch00_C00_Z0*.tif'))) #find all z planes in folder
+
+    zpln = tifffile.TiffFile(zplns[0])
+    h5_filename = os.path.join(dst, vol.brainname + '.h5')
+    
+    #create memory mapped array
+    fp = np.memmap(os.path.join(dst, vol.brainname + '.dat'), dtype='uint16', mode='w+', shape = (len(zplns), zpln.asarray().shape[0], zpln.asarray().shape[1]))
+    
+    #populate h5 file with each zpln
+    for i, fn in enumerate(zplns):
+        im = tifffile.TiffFile(fn)
+        fp[i, :, :] = im.asarray()
+            
+    #save out h5 file    
+    with h5py.File(h5_filename) as h5_file:
+        h5_file.create_dataset('/main', data = fp)
+   
+    del fp
+    
+    sys.stdout.write('\n\nHDF5 file generated for {}\n       in {} minutes\n\n'.format(vol.brainname, (time.time() - start)/60))
+         
