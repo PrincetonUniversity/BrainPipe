e4f97de0f113605ebd2643a24f2371a7a724823b
zahramansoor <zahradhanerawala@gmail.com>
2018-10-04 17:15:26 -0400

    adding jukebox paths

--- basic_utils/wanglab/sweep.py
+++ basic_utils/wanglab/sweep.py
@@ -5,6 +5,7 @@ Created on Wed Oct  3 11:39:37 2018
 
 @author: wanglab
 """
+
 from __future__ import division
 import os, numpy as np, sys, multiprocessing as mp, time
 from scipy import ndimage
@@ -16,9 +17,10 @@ import subprocess as sp
 from tools.conv_net.functions.bipartite import pairwise_distance_metrics
 
 if __name__ == '__main__':
+    
     #transfer from globus
     src = '/tigress/zmd/wang/zahra/3dunet_cnn/experiments/20181001_zd_train/forward'
-    dest = 'Documents/python/data/20181001_zd_train/forward' #wanglab endpoint assumes default directory of /home/wanglab/
+    dest = '/jukebox/LightSheetTransfer/cnn/zmd' #wanglab endpoint assumes default directory of /home/wanglab/
     label = 'model_20181001'
     transfer(src, dest, label, other_endpoint = False)
         
@@ -33,23 +35,23 @@ if __name__ == '__main__':
 #            'JGANNOTATION_20170115_tp_bl6_lob6a_500r_01_647_010na_z7d5um_75_msec_10povlp_ch00_C00_425-460_00', 
 #            '20170204_tp_bl6_cri_1000r_02_1hfds_647_0010na_25msec_z7d5um_10povlap_ch00_z200-400_y1350-1700_x3100-3450']
     
-    #set relevant paths
-    pth = '/home/wanglab/'+dest
+    #after transfer, set relevant paths
+    pth = '/jukebox/LightSheetTransfer/cnn/zmd/20180929_360000chkpnt_xy160z20/forward'
     points_dict = load_dictionary('/jukebox/wang/pisano/conv_net/annotations/all_better_res/h129/filename_points_dictionary.p')
     
+#******************************************************************************************************************************************    
     #initialise empty vectors
-    tps = []; fps = []; fns = []
-    
+    tps = []; fps = []; fns = []    
     #iterates through forward pass output
     for dset in os.listdir(pth):
         impth = os.path.join(pth, dset)
-        predicted = probabiltymap_to_centers_thresh(impth, threshold = (0.4, 0.99))
+        predicted = probabiltymap_to_centers_thresh(impth, threshold = (0.65, 1))
         
         print '\n   Finished finding centers for {}, calculating statistics\n'.format(dset)
         
-        ground_truth = points_dict[dset[:-20]+'.npy'] #modifying file names so they match with original data
+        ground_truth = points_dict[dset[:-21]+'.npy'] #modifying file names so they match with original data
         
-        paired,tp,fp,fn = pairwise_distance_metrics(ground_truth, predicted, cutoff = 10) #returns true positive = tp; false positive = fp; false negative = fn
+        paired,tp,fp,fn = pairwise_distance_metrics(ground_truth, predicted, cutoff = 20) #returns true positive = tp; false positive = fp; false negative = fn
        
         tps.append(tp); fps.append(fp); fns.append(fn) #append matrix to save all values to calculate f1 score
        
@@ -66,22 +68,23 @@ def transfer(src, dest, label, other_endpoint = False):
         src = tigress source path
         dest = destination path
         label = name of transfer
-        other_endpoint = False; assumes transfer to wanglab A84D computer unless jukebox/other endpoint keys specified
+        other_endpoint = False; assumes transfer to LightSheetTransfer unless jukebox/other endpoint keys specified
     '''    
     #for globus transfer, endpoint keys: 
     #tigress = 'a9df83d2-42f0-11e6-80cf-22000b1701d1' #transferring from
     #wanglab = 'f7949748-c728-11e8-8c57-0a1d4c5c824a' #transferring to
-        
+    #jukebox = '6ce834d6-ff8a-11e6-bad1-22000b9a448b'    
+    
     tigress_pth = 'a9df83d2-42f0-11e6-80cf-22000b1701d1:'+src
     if not other_endpoint:
-        local_pth = 'f7949748-c728-11e8-8c57-0a1d4c5c824a:'+dest
+        lst_pth = '6ce834d6-ff8a-11e6-bad1-22000b9a448b:'+dest
     else:
-        local_pth = other_endpoint+':'+dest
+        lst_pth = other_endpoint+':'+dest
     
-    sp.call(['globus', 'transfer', tigress_pth, local_pth, '--recursive', '--label', label]) #run command line call
+    sp.call(['globus', 'transfer', tigress_pth, lst_pth, '--recursive', '--label', label]) #run command line call
     
     
-def probabiltymap_to_centers_thresh(src, threshold = (0.5, 0.99), numZSlicesPerSplit = 200, overlapping_planes = 40, cores = 4, return_pixels = False, verbose = True, structure_rank_order = 2):
+def probabiltymap_to_centers_thresh(src, threshold = (0.1,1), numZSlicesPerSplit = 200, overlapping_planes = 40, cores = 4, return_pixels = False, verbose = True, structure_rank_order = 2):
     '''
     by tpisano
     
