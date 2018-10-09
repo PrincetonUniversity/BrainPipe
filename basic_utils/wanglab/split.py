4a98fed8ea1198db893debf14aa0535a55d9ce50
zahramansoor <zahradhanerawala@gmail.com>
2018-10-09 14:36:15 -0400

    tiger2push

--- basic_utils/wanglab/split.py
+++ basic_utils/wanglab/split.py
@@ -9,6 +9,7 @@ Created on Fri Oct  5 15:00:47 2018
 from sklearn.model_selection import train_test_split
 import os, shutil
 
+data_pth = '/home/wanglab/Documents/python/data/training_data'
 pth = '/home/wanglab/Documents/python/data/training_data/inputRawImages'
 lbl = '/home/wanglab/Documents/python/data/training_data/inputLabelImages'
 
@@ -18,6 +19,7 @@ raw = [xx[:-19] for xx in raw] #because we add the extension back in sampler, ca
 #-18 for h5s, -19 for tifs
 #FIXME - do regex
 
+#%70-20-10 train-validation-test
 train, test = train_test_split(raw, test_size = 0.3, train_size = 0.7, random_state = 1)   
 val, test = train_test_split(test, test_size = 0.666, train_size = 0.333, random_state = 1)
 
@@ -25,26 +27,26 @@ val, test = train_test_split(test, test_size = 0.666, train_size = 0.333, random
 #test
 for i in test: #move raw and labels to appropriate bins
     t = os.path.join(lbl, i+'_inputLabelImages-segmentation.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/test/label')
+    shutil.move(t, data_pth+'/test/label')
     
 for i in test:
     t = os.path.join(pth, i+'_inputRawImages.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/test/raw')
+    shutil.move(t, data_pth+'/test/raw')
     
 #train
 for i in train: 
     t = os.path.join(lbl, i+'_inputLabelImages-segmentation.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/train/label')
+    shutil.move(t, data_pth+'/train/label')
     
 for i in train:
     t = os.path.join(pth, i+'_inputRawImages.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/train/raw')
+    shutil.move(t, data_pth+'/train/raw')
     
 #val
 for i in val: 
     t = os.path.join(lbl, i+'_inputLabelImages-segmentation.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/val/label')
+    shutil.move(t, data_pth+'/val/label')
     
 for i in val:
     t = os.path.join(pth, i+'_inputRawImages.tif')
-    shutil.move(t, '/home/wanglab/Documents/python/data/training_data/val/raw')
+    shutil.move(t, data_pth+'/val/raw')
