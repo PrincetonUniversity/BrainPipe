e12805814636a2a3869788dcc7bd08f372eb59a4
zahramansoor <zahramansoor@github.com>
2018-10-16 15:36:13 -0400

    autopush

--- run_neurotorch.py
+++ run_neurotorch.py
@@ -6,7 +6,7 @@ Created on Tue Oct  2 13:14:05 2018
 @author: wanglab
 """
 
-from neurotorch.datasets.dataset import TiffVolume, LargeTiffVolume, Volume
+from neurotorch.datasets.filetypes import TiffVolume
 from neurotorch.core.trainer import Trainer
 from neurotorch.nets.RSUNet import RSUNet
 from neurotorch.core.predictor import Predictor
@@ -21,8 +21,8 @@ def main():
     
     net = RSUNet()  # Initialize the U-Net architecture
 
-    inputs = TiffVolume('/home/wanglab/Documents/python/3dunet_data/neurotorch_training_data/train/raw/20170115_tp_bl6_lob6a_1000r_647_010na_z7d5um_125msec_10povlp_ch00_626-675_00_inputRawImages.tif')  # Create a volume containing inputs
-    labels = TiffVolume('/home/wanglab/Documents/python/3dunet_data/neurotorch_training_data/train/label/20170115_tp_bl6_lob6a_1000r_647_010na_z7d5um_125msec_10povlp_ch00_626-675_00_inputLabelImages-segmentation.tif')  # Create a volume containing labels
+    inputs = TiffVolume('/home/wanglab/Documents/training_data/train/raw/20170115_tp_bl6_lob6a_1000r_647_010na_z7d5um_125msec_10povlp_ch00_626-675_03_inputRawImages.tif')  # Create a volume containing inputs
+    labels = TiffVolume('/home/wanglab/Documents/training_data/train/label/20170115_tp_bl6_lob6a_1000r_647_010na_z7d5um_125msec_10povlp_ch00_626-675_03_inputLabelImages-segmentation')  # Create a volume containing labels
 
     print ('**************************************************************************************\n\n      Inputs and Labels read correctly. Starting training...\n\n') #zmd added
     
