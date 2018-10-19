2153e2a69c67862dda7e985ecf82c94cc92e2cbf
zahramansoor <zahramansoor@github.com>
2018-10-19 17:51:30 -0400

    autopush
    Former-commit-id: 8b0764136bcef2f8f5f4bb07ae21cd7ac3490f67

--- basic_utils/wanglab/validation.py
+++ basic_utils/wanglab/validation.py
@@ -4,28 +4,56 @@ Created on Mon Oct 15 16:44:04 2018
 
 @author: Zahra
 """
-import re
+
+import re, os
 import matplotlib.pyplot as plt
 
-#read text file output from PyTorchUtils
-test = ''
-with open('cnn_train_20181009_1528952.out', 'r') as searchfile:
-    for line in searchfile:
-        if 'TEST:' in line: #finds all lines with test
-            test += line
-    searchfile.close()
 
-n = re.compile("(?<='soma_label':\s)(\d+.\d+)") #finds loss values in test lines
-loss = n.findall(test)
-loss = [round(float(xx), 5) for xx in loss if str(xx)] 
+def plot_val_curve(pth, exclude_initial = False):
+    '''Function to plot validation data loss value from .out file from training on tiger2
+    Inputs:
+        pth = directory of .out files from training
+    Returns:
+        pdf of plot, in the path directory
+    '''
+    
+    #initialise things
+    test = ''
+    
+    out = os.listdir(pth)
+    pths = [os.path.join(pth, f) for f in out if f[-4:] == '.out']; pths.sort() #sort so trainings are in order of time performed
+    
+    #read text file output from PyTorchUtils
+    for src in pths:
+        with open(src, 'r') as searchfile:
+            for line in searchfile:
+                if 'TEST:' in line: #finds all lines with test
+                    test += line
+            searchfile.close()
 
-#plot
-plt.rcParams.update({'font.size': 8})
-plt.figure()
-plt.plot(loss, 'r')
-plt.ylim(0, 0.02)
-plt.xlabel('# of iterations in thousands')
-plt.ylabel('loss value')
-plt.title('3D U-net validation curve for H129')          
-plt.savefig('val_zoom.pdf', dpi = 300)
-plt.close()
+    #regex    
+    n = re.compile("(?<='soma_label':\s)(\d+.\d+)") #finds loss values in test lines
+    loss = n.findall(test)
+    loss = [round(float(xx), 5) for xx in loss if str(xx)] #write into numerical vector
+    
+    #plot
+    if exclude_initial:
+        plt.rcParams.update({'font.size': 8})
+        plt.figure()
+        plt.plot(loss[1:], 'r')
+        plt.ylim(0, 0.0015)
+        plt.xlabel('# of iterations in thousands')
+        plt.ylabel('loss value')
+        plt.title('3D U-net validation curve for H129')          
+        plt.savefig(os.path.join(pth, 'val_zoom_initial_test_omitted.pdf'), dpi = 300)
+        plt.close()
+    else:
+        plt.rcParams.update({'font.size': 8})
+        plt.figure()
+        plt.plot(loss, 'r')
+        plt.ylim(0, 0.02)
+        plt.xlabel('# of iterations in thousands')
+        plt.ylabel('loss value')
+        plt.title('3D U-net validation curve for H129')          
+        plt.savefig(os.path.join(pth, 'val_zoom'), dpi = 300)
+        plt.close()    
--- run_neurotorch.py
+++ run_neurotorch.py
@@ -16,38 +16,25 @@ from skimage.external import tifffile
 
 def main():
     
-    print ('\n\n      Using torch version: {}\n\n'.format(torch.__version__)) #zmd added 
+    print ('\n\n      Using torch version: {}\n\n'.format(torch.__version__)) #check torch version is correct
     
-    net = torch.nn.DataParallel(RSUNet())  # Initialize the U-Net architecture - zmd modified
+    net = torch.nn.DataParallel(RSUNet())  # Initialize the U-Net architecture
 
-    inputs = TiffVolume('/home/wanglab/Documents/training_data/train/raw/20170204_tp_bl6_cri_1000r_02_1hfds_647_0010na_25msec_z7d5um_10povlap_ch00_z200-400_y1350-1700_x3100-3450_inputRawImages.tif')  # Create a volume containing inputs
+    inputs = TiffVolume('/home/wanglab/Documents/python/NeuroTorch/data')  # Create a volume to feed into predictor
     inputs.__enter__()
-    
-#    labels = TiffVolume('/home/wanglab/Documents/training_data/train/label/20170204_tp_bl6_cri_1000r_02_1hfds_647_0010na_25msec_z7d5um_10povlap_ch00_z200-400_y1350-1700_x3100-3450_inputLabelImages-segmentation.tif')  # Create a volume containing labels
-#
-#    print ('**************************************************************************************\n\n      Inputs and Labels read correctly. Starting training...\n\n') #zmd added
-#    
-#    trainer = Trainer(net, inputs, labels,  # Setup a network trainer
-#                      max_epochs=100, gpu_device=0)
-#
-#    # Set the trainer to add a checkpoint every 50 epochs
-#    trainer = CheckpointWriter(trainer, checkpoint_dir='.', 
-#                               checkpoint_period=5)
-#
-#    trainer.run_training()  # Start training
 
-    outputs = TiffVolume('/home/wanglab/Documents/python/NeuroTorch/data') # Create a volume for predictions
-    outputs.__enter__()
+#    outputs = TiffVolume('/home/wanglab/Documents/python/NeuroTorch/data') # Create a volume for predictions
+#    outputs.__enter__()
     
-    print ('*********************************************************************************************\n\n\
-           Finished training :) Starting predictions...\n\n') #zmd added
+    print ('*******************************************************************************\n\n\
+           Finished training :) Starting predictions...\n\n') 
     
     # Setup a predictor for computing outputs
     predictor = Predictor(net, checkpoint='/jukebox/wang/zahra/conv_net/20181009_zd_train/models/model715000.chkpt', gpu_device=0) #zmd added gpu device
 
     predictor.run(inputs, outputs, batch_size=1)  # Run prediction
 
-    print ('*********************************************************************************************\n\n\
+    print ('*******************************************************************************\n\n\
            Finishing predictions :) Saving... \n\n') #zmd added
     
 #    print (outputs.get().getArray())
