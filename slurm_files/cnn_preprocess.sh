91117968d47326ea6376c4fb79e812276776d74c
zahramansoor <zahramansoor@github.com>
2019-06-24 18:10:01 -0400

    autopush

--- slurm_files/cnn_preprocess.sh
+++ slurm_files/cnn_preprocess.sh
@@ -3,8 +3,8 @@
 #SBATCH -p all                # partition (queue)
 #SBATCH -c 1                      # number of cores
 #SBATCH -t 10                # time (minutes)
-#SBATCH -o /scratch/zmd/logs/cnn_preprocess_%j.out        # STDOUT #add _%a to see each array job
-#SBATCH -e /scratch/zmd/logs/cnn_preprocess_%j.err        # STDERR #add _%a to see each array job
+#SBATCH -o logs/cnn_preprocess_%j.out        # STDOUT #add _%a to see each array job
+#SBATCH -e logs/cnn_preprocess_%j.err        # STDERR #add _%a to see each array job
 
 echo "In the directory: `pwd` "
 echo "As the user: `whoami` "
@@ -19,15 +19,15 @@ module load anacondapy/5.3.1
 echo "Experiment name:" "`pwd`"
 
 #generate memmap array of full size cell channel data
-OUT0=$(sbatch slurm_scripts/cnn_step0.sh "`pwd`") 
+OUT0=$(sbatch slurm_files/cnn_step0.sh "`pwd`") 
 echo $OUT0
 
 #generate chunks for cnn input
-OUT1=$(sbatch --dependency=afterany:${OUT0##* } --array=0-130 slurm_scripts/cnn_step1.sh "`pwd`") 
+OUT1=$(sbatch --dependency=afterany:${OUT0##* } --array=0-130 slurm_files/cnn_step1.sh "`pwd`") 
 echo $OUT1
 
 #check if correct number of patches were made
-OUT2=$(sbatch --dependency=afterany:${OUT1##* } slurm_scripts/cnn_step1_check.sh "`pwd`") 
+OUT2=$(sbatch --dependency=afterany:${OUT1##* } slurm_files/cnn_step1_check.sh "`pwd`") 
 echo $OUT2
 
 #functionality
--- slurm_files/cnn_step1.sh
+++ slurm_files/cnn_step1.sh
@@ -3,8 +3,8 @@
 #SBATCH -p all                # partition (queue)
 #SBATCH -c 8                      # number of cores
 #SBATCH -t 200                # time (minutes)
-#SBATCH -o /scratch/zmd/logs/array_jobs/cnn_step1_chnk%a_%j.out        # STDOUT #add _%a to see each array job
-#SBATCH -e /scratch/zmd/logs/array_jobs/cnn_step1_chnk%a_%j.err        # STDERR #add _%a to see each array job
+#SBATCH -o logs/array_jobs/cnn_step1_chnk%a_%j.out        # STDOUT #add _%a to see each array job
+#SBATCH -e logs/array_jobs/cnn_step1_chnk%a_%j.err        # STDERR #add _%a to see each array job
 #SBATCH --contiguous #used to try and get cpu mem to be contigous
 #SBATCH --mem 30000 #30 gbs
 
