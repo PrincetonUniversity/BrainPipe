f5babb6bcea5c4637a15a500094cd410f4bc9f7c
zahramansoor <zahramansoor@github.com>
2019-06-26 10:01:19 -0400

    autopush

--- tools/utils/directorydeterminer.py
+++ tools/utils/directorydeterminer.py
@@ -13,52 +13,25 @@ Alternative would be to softlink paths of different computers to have the same p
 import os, socket, collections
 
 def directorydeterminer():
-    '''Function to allow for different paths to the same server. This allows for working locally and on cluster.
+    """Function to allow for different paths to the same server. This allows for working locally and on cluster.
 
     EDIT THIS to fit your paths.
-    '''
-    if socket.gethostname() == 'spock-login.pni.princeton.edu':
-        systemdirectory= '/jukebox/'
-    elif socket.gethostname() == 'pni-3cnxk02':
-        systemdirectory= '/home/tpisano/'
-    elif socket.gethostname() == 'PNI-1867WTQ2':
-        systemdirectory= '/jukebox/'
-    elif 'HOSTNAME' in os.environ.keys() and os.environ['HOSTNAME'] == 'della5.princeton.edu':
-        systemdirectory= '/tigress/tpisano/'
-    elif socket.gethostname() == 'pniadmin-Precision-WorkStation-T3500':
-        systemdirectory='/home/yisizhang'    
-    elif socket.gethostname() == 'wanglab-cr8rc42-ubuntu':
-        systemdirectory=  '/home/wanglab/'
-    elif socket.gethostname() == 'tiger1':
-        systemdirectory=  '/tigress/tpisano/' 
-    elif socket.gethostname() == 'pni_3cnxk02':
-        systemdirectory=  '/home/tpisano/' 
-    elif os.getcwd()[:6] == '/Users':
-        systemdirectory= '/Volumes/'
-    elif os.getcwd()[:8] == '/Volumes':
-        systemdirectory= '/Volumes/'
-    elif os.getcwd()[:7] == '/home/w':
-        systemdirectory= '/home/wanglab/'
-    elif os.getcwd()[:7] == '/home/t':
-        systemdirectory= '/home/tpisano/'
-    elif os.getcwd()[:8] == '/jukebox':
-        systemdirectory= '/jukebox/'
-    elif os.getcwd()[:4] == '/mnt':
-        systemdirectory= '/jukebox/'
-    elif os.getcwd()[:8] == '/tigress':
-        systemdirectory= '/tigress/tpisano/'#'/scratch/gpfs/tpisano/'
-        #systemdirectory= '/tigress/tpisano/' 
-    elif socket.gethostname() == 'ellia-VirtualBox':
-        systemdirectory= '/jukebox/'
-    if socket.gethostname() == 'PNI-1867vwtq2':
-        systemdirectory= '/jukebox/'
+    """
+    if socket.gethostname() == "spock-login.pni.princeton.edu":
+        systemdirectory= "/jukebox/"
+    elif socket.gethostname() == "pni-3cnxk02":
+        systemdirectory= "/home/tpisano/"
+    elif socket.gethostname() == "PNI-1867WTQ2":
+        systemdirectory= "/jukebox/"
+    if socket.gethostname() == "PNI-1867vwtq2":
+        systemdirectory= "/jukebox/"
     return systemdirectory
 
 
 def pth_update(item):    
-    '''simple way to update dictionary, list, or str for local path, should be recursive for dicts
-    '''
-    for prefix in ['/jukebox/', '/mnt/bucket/labs/', '/home/wanglab/', '/home/tpisano/', '/scratch/gpfs/tpisano/', '/tigress/tpisano/']:
+    """simple way to update dictionary, list, or str for local path, should be recursive for dicts
+    """
+    for prefix in ["/jukebox/", "/mnt/bucket/labs/", "/home/wanglab/", "/home/tpisano/", "/scratch/gpfs/tpisano/", "/tigress/tpisano/"]:
         if type(item) == dict:
             for keys, values in item.items():
                 if type(values) == str:
@@ -95,6 +68,7 @@ def dict_recursion(d, prefix):
                 if prefix in v: v = os.path.join(directorydeterminer(), v[v.rfind(prefix)+len(prefix):])
         nd[k] = v
     return nd
+
 def update_list(lst, prefix):
     nlst = []
     for v in lst:
