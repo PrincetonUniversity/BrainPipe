e17e3040cad6e1e8371a2705b43d9373dcf5ae71
zahramansoor <zahramansoor@github.com>
2019-12-19 17:43:39 -0500

    autopush

--- run_tracing.py
+++ run_tracing.py
@@ -21,18 +21,16 @@ systemdirectory=directorydeterminer()
 #"injch" = channels(s) to quantify injection site
 #e.g.: inputdictionary={path_1: [["regch", "00"]], path_2: [["cellch", "00"], ["injch", "01"]]} ###create this dictionary variable BEFORE params
 inputdictionary={
-os.path.join(systemdirectory, "LightSheetTransfer/kelly/201912_cfos/raw_data/191217_m57208_demons_20190910_488_008na_1hfds_z5um_100msec_15-59-26"): 
-    [["regch", "00"]],
-os.path.join(systemdirectory, "LightSheetTransfer/kelly/201912_cfos/raw_data/191217_m57208_demons_20190910_555_008na_1hfds_z5um_100msec_15-45-38"): 
-    [["cellch", "00"]],
+os.path.join(systemdirectory, "LightSheetTransfer/jduva/191218_stefano_k01_201912_cmdii_1d3x_488_555_016na_1hfds_z10um_100msec_17-19-59"): 
+    [["regch", "00"], ["injch", "01"]],
 }
 
 ####Required inputs
 params={
 "systemdirectory":  systemdirectory, #don"t need to touch
 "inputdictionary": inputdictionary, #don"t need to touch
-"outputdirectory": os.path.join(systemdirectory, "LightSheetTransfer/kelly/201912_cfos/processed/m57208_demons_20190910"),
-"xyz_scale": (5, 5, 5), #(5.0,5.0,3), #micron/pixel: 5.0um/pix for 1.3x; 1.63um/pix for 4x
+"outputdirectory": os.path.join(systemdirectory, "LightSheetData/falkner-mouse/stefan/processed/mousek01"),
+"xyz_scale": (5, 5, 10), #(5.0,5.0,3), #micron/pixel: 5.0um/pix for 1.3x; 1.63um/pix for 4x
 "tiling_overlap": 0.00, #percent overlap taken during tiling
 "stitchingmethod": "blending", #"terastitcher", blending see below for details
 "AtlasFile": os.path.join(systemdirectory, "LightSheetTransfer/atlas/allen_atlas/average_template_25_sagittal_forDVscans.tif"),
