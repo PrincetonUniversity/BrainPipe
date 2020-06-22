37940b430f7491ab765159af755de94c71aa1053
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-22 13:42:03 -0400

    changing gpu use based on convo with Zahra in run_exp

--- tools/conv_net/pytorchutils/slurm_scripts/run_exp.sh
+++ tools/conv_net/pytorchutils/slurm_scripts/run_exp.sh
@@ -3,14 +3,13 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=4
 #SBATCH --ntasks-per-socket=2
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --contiguous
 #SBATCH --mem=20000 #20gbs
 #SBATCH -t 8500                 # time (minutes)
-#SBATCH -o /scratch/gpfs/zmd/logs/cnn_train.out
-#SBATCH -e /scratch/gpfs/zmd/logs/cnn_train.err
+#SBATCH -o /scratch/gpfs/ejdennis/logs/cnn_train.out
+#SBATCH -e /scratch/gpfs/ejdennis/logs/cnn_train.err
 
 module load cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1 anaconda3/5.3.1
 . activate 3dunet
-python run_exp.py 20200316_peterb_zd_train models/RSUNet.py samplers/soma.py augmentors/flip_rotate.py --batch_sz 4 --gpus 0
-
+python run_exp.py 20200622_ed_train models/RSUNet.py samplers/soma.py augmentors/flip_rotate.py --batch_sz 4 --gpus 0
