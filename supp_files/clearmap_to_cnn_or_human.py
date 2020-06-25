98315535b8fcd0f90515c262e223fede8a057395
emilyjanedennis <emilyjanedennis@gmail.com>
2020-06-25 16:32:05 -0400

    added header

---
+++ supp_files/clearmap_to_cnn_or_human.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 26 2020
+@author: emilyjanedennis
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from lightsheet_BrainPipe.tools.conv_net.utils.io import pairwise_distance_metrics, read_roi_zip
+
+if __name__ == "__main__":
