7e7cbfd2538ced8ec448b9ccc7a5f47622753d9a
zahramansoor <zahramansoor@github.com>
2020-07-13 18:38:30 -0400

    autopush

--- run_tracing.py
+++ run_tracing.py
@@ -21,22 +21,22 @@ systemdirectory=directorydeterminer()
 #"injch" = channels(s) to quantify injection site
 #e.g.: inputdictionary={path_1: [["regch", "00"]], path_2: [["cellch", "00"], ["injch", "01"]]} ###create this dictionary variable BEFORE params
 inputdictionary={
-os.path.join(systemdirectory, "wang/mkislin/lightsheet_brains/201903_cntnap2_tsc1_ai148/todo/201707_mk63_488_647_014na_1hfsds_z10um_300msec_ch00"): 
+os.path.join(systemdirectory, "LightSheetData/brodyatlas/raw_data/200709_x050_1_1x_488_016na_1hfls_z5um_50msec_20povlp_16-47-57"): 
     [["regch", "00"]],
-os.path.join(systemdirectory, "wang/mkislin/lightsheet_brains/201903_cntnap2_tsc1_ai148/todo/201707_mk63_488_647_014na_1hfsds_z10um_300msec_ch01"): 
-    [["injch", "00"]]
+os.path.join(systemdirectory, "LightSheetData/brodyatlas/raw_data/200709_x050_1_1x_647_016na_1hfds_z5um_150msec_20povlp_15-21-23"): 
+    [["cellch", "00"]]
 }
 
 ####Required inputs
 params={
 "systemdirectory":  systemdirectory, #don"t need to touch
 "inputdictionary": inputdictionary, #don"t need to touch
-"outputdirectory": os.path.join(systemdirectory, "wang/mkislin/lightsheet_brains/201903_cntnap2_tsc1_ai148/201707_mk63"),
-"xyz_scale": (5,5,10), #(5.0,5.0,3), #micron/pixel: 5.0um/pix for 1.3x; 1.63um/pix for 4x
-"tiling_overlap": 0.00, #percent overlap taken during tiling
+"outputdirectory": os.path.join(systemdirectory, "LightSheetData/brodyatlas/processed/x050"),
+"xyz_scale": (5.91,5.91,5), #(5.0,5.0,3), #micron/pixel: 5.0um/pix for 1.3x; 1.63um/pix for 4x
+"tiling_overlap": 0.20, #percent overlap taken during tiling
 "stitchingmethod": "blending", #"terastitcher", blending see below for details
-"AtlasFile": os.path.join(systemdirectory, "LightSheetTransfer/atlas/allen_atlas/average_template_25_sagittal_forDVscans.tif"),
-"annotationfile": os.path.join(systemdirectory, "LightSheetTransfer/atlas/allen_atlas/annotation_template_25_sagittal_forDVscans.tif"), ###path to annotation file for structures
+"AtlasFile": os.path.join(systemdirectory, "LightSheetData/brodyatlas/atlas/for_registration_to_lightsheet/WHS_SD_rat_T2star_v1.01_atlas.tif"),
+"annotationfile": os.path.join(systemdirectory, "LightSheetData/brodyatlas/atlas/for_registration_to_lightsheet/WHS_SD_rat_atlas_v3_annotation.tif"), ###path to annotation file for structures
 "blendtype": "sigmoidal", #False/None, "linear", or "sigmoidal" blending between tiles, usually sigmoidal; False or None for images where blending would be detrimental
 "intensitycorrection": True, #True = calculate mean intensity of overlap between tiles shift higher of two towards lower - useful for images where relative intensity is not important (i.e. tracing=True, cFOS=False)
 "resizefactor": 3, ##in x and y #normally set to 5 for 4x objective, 3 for 1.3x obj
