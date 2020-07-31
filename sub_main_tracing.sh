4075250cbbe4c86e7196d7412b1e4f8d7e8d7344
emilyjanedennis <emilyjanedennis@gmail.com>
2020-07-31 16:22:16 -0400

    rm -p headers to make running from Brody node easier

--- sub_main_tracing.sh
+++ sub_main_tracing.sh
@@ -1,6 +1,5 @@
 #!/bin/env bash
 #
-#SBATCH -p all                # partition (queue)
 #SBATCH -n 1                      # number of cores
 #SBATCH -t 20                 # time (minutes)
 #SBATCH -o logs/outmain_registration.out        # STDOUT
@@ -22,31 +21,31 @@ module load elastix/4.8
 . activate lightsheet
 
 #set up dictionary and save
-OUT0=$(sbatch --array=0 slurm_files/step0.sh) 
+OUT0=$(sbatch --array=0 slurm_files/step0.sh)
 echo $OUT0
 
 #process zplns, assumes with tracing you are using terastitcher
-OUT1=$(sbatch --dependency=afterany:${OUT0##* } --array=0-2 slurm_files/step1.sh) 
+OUT1=$(sbatch --dependency=afterany:${OUT0##* } --array=0-2 slurm_files/step1.sh)
 echo $OUT1
 
 #combine stacks into single tifffiles
-OUT2=$(sbatch --dependency=afterany:${OUT1##* } --array=0-3 slurm_files/step2.sh) 
+OUT2=$(sbatch --dependency=afterany:${OUT1##* } --array=0-3 slurm_files/step2.sh)
 echo $OUT2
 
 #generate memmap array of full size cell channel data
-OUT4=$(sbatch --dependency=afterany:${OUT1##* } slurm_files/cnn_step0.sh "`pwd`") 
+OUT4=$(sbatch --dependency=afterany:${OUT1##* } slurm_files/cnn_step0.sh "`pwd`")
 echo $OUT4
 
 #generate chunks for cnn input
-OUT5=$(sbatch --dependency=afterany:${OUT4##* } --array=0-130 slurm_files/cnn_step1.sh "`pwd`") 
+OUT5=$(sbatch --dependency=afterany:${OUT4##* } --array=0-130 slurm_files/cnn_step1.sh "`pwd`")
 echo $OUT5
 
 #check if correct number of patches were made
-OUT6=$(sbatch --dependency=afterany:${OUT5##* } slurm_files/cnn_step1_check.sh "`pwd`") 
+OUT6=$(sbatch --dependency=afterany:${OUT5##* } slurm_files/cnn_step1_check.sh "`pwd`")
 echo $OUT6
 
 #run elastix
-OUT3=$(sbatch --dependency=afterany:${OUT2##* } --array=0-2 slurm_files/step3.sh) 
+OUT3=$(sbatch --dependency=afterany:${OUT2##* } --array=0-2 slurm_files/step3.sh)
 echo $OUT3
 
 
