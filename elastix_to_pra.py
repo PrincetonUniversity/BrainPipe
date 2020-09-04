57220b27e1fd3b7f75cca79d543d1fcc21e647a3
emilyjanedennis@gmail.com <emilyjanedennis@gmail.com>
2020-09-04 18:35:00 -0400

    end of day Linux cleanup

--- elastix_to_pra.py
+++ elastix_to_pra.py
@@ -13,26 +13,19 @@ import sys
 from scipy.ndimage import zoom
 sys.path.append("/home/emilyjanedennis/Desktop/GitHub/rat_BrainPipe/")
 
-# chon to PMA
+# k315 and k310 to MRIr
 
-mv = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/Chon-Allen-brain.tif"
-fx = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/PMA.tif"
-outputfilename = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/Chon_for_PMA-reg.tif"
-outputdirectory = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/Chon_to_PMA"
 param_fld = "/home/emilyjanedennis/Desktop/brains/w122/parameterfolder"
 
-# need to make MRI atlas larger (~140% of atlas?) to transform to PRA
-atlasbrain = tif.imread(mv)
-pra = tif.imread(fx)
-zf, yf, xf = (pra.shape[0]/atlasbrain.shape[0])*1.4, (pra.shape[1] /
-                                                      atlasbrain.shape[1])*1.4, (pra.shape[2]/atlasbrain.shape[2])*1.4
-atlasbrain_for_pra = zoom(atlasbrain, (zf, yf, xf), order=1)
-
-tif.imsave(outputfilename, atlasbrain_for_pra.astype("uint16"))
+mv = "/home/emilyjanedennis/Desktop/brains/k315_1_1x_555_016na_1hfds_z10um_50msec_20povlp_resized_ch00.tif"
+fx = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/tiffs/WHS_SD_rat_T2star_v1.01_atlas.tif"
+outputfilename = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/enlarged_tiffs/k315_for_MRIr.tif"
+outputdirectory = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/output_dirs/k315_to_MRIr"
 
 if not os.path.exists(outputdirectory):
     os.mkdir(outputdirectory)
 
 params = [os.path.join(param_fld, xx) for xx in os.listdir(param_fld)]
 
-e_out, transformfiles = elastix_command_line_call(fx, outputfilename, out, params)
+e_out, transformfiles = elastix_command_line_call(fx, outputfilename, outputdirectory, params)
+
