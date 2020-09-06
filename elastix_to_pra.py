abbc6542f6a6ade77734bcbc0a3a2d4e2e45474c
emilyjanedennis@gmail.com <emilyjanedennis@gmail.com>
2020-09-06 17:46:09 -0400

    edits to work on female to male operationally defined

--- elastix_to_pra.py
+++ elastix_to_pra.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -12,15 +13,28 @@ import tifffile as tif
 import sys
 from scipy.ndimage import zoom
 sys.path.append("/home/emilyjanedennis/Desktop/GitHub/rat_BrainPipe/")
-
-# k315 and k310 to MRIr
+src = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet"
 
 param_fld = "/home/emilyjanedennis/Desktop/brains/w122/parameterfolder"
 
-mv = "/home/emilyjanedennis/Desktop/brains/k315_1_1x_555_016na_1hfds_z10um_50msec_20povlp_resized_ch00.tif"
-fx = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/tiffs/WHS_SD_rat_T2star_v1.01_atlas.tif"
-outputfilename = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/enlarged_tiffs/k315_for_MRIr.tif"
-outputdirectory = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/output_dirs/k315_to_MRIr"
+fx = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/tiffs/median_image.tif"
+mv = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/tiffs/median_female_f003seed_image.tif"
+outputfilename = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/enlarged_tiffs/PRAf_for_PRAm.tif"
+outputdirectory = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/output_dirs/PRAf_to_PRAm"
+
+# need to make moving larger (~140% seems to work well?) to transform to fixed
+moving = tif.imread(mv)
+fixed = tif.imread(fx)
+zf, yf, xf = (fixed.shape[0]/moving.shape[0])*1.4, (
+    fixed.shape[1] /
+    moving.shape[1])*1.4, (fixed.shape[2]/moving.shape[2])*1.4
+print("\nzooming...")
+moving_for_fixed = zoom(moving, (zf, yf, xf), order=0)
+
+# saved out volume
+print("\nsaving zoomed volume...")
+tif.imsave(outputfilename,moving_for_fixed.astype("uint16"))
+
 
 if not os.path.exists(outputdirectory):
     os.mkdir(outputdirectory)
