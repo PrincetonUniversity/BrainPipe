71acac5af828e685ab80722924f06b4ad11722b9
emilyjanedennis@gmail.com <emilyjanedennis@gmail.com>
2020-10-08 17:58:04 -0400

    end of day Linux cleanup

--- tools/analysis/transformix_image_without_zoom.py
+++ tools/analysis/transformix_image_without_zoom.py
@@ -28,28 +28,14 @@ from scipy.ndimage.interpolation import zoom
 
 # setting paths
 src = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/"
-# ann = "/home/emilyjanedennis/Desktop/for_registration_to_lightsheet/10grid.tif"
-fx = os.path.join(src, "tiffs/WHS_SD_rat_T2star_v1.01_atlas.tif")
-mv = os.path.join(src, "tiffs/SIGMA_sagittal_ann.tif")
-enlargedfilename= os.path.join(src, "enlarged_tiffs/SIGMA_ann_forMRIr.tif")
 
-dst = os.path.join(src,"output_dirs/SIGMA_ann_toMRIr")
-makedir(dst)
-transformfilepath = os.path.join(src, "transform_files/SIGMA_forMRIr")
-moving = tif.imread(mv)
-fixed = tif.imread(fx)
-zf, yf, xf = (fixed.shape[0]/moving.shape[0])*1.4, (
-    fixed.shape[1] /
-    moving.shape[1])*1.4, (fixed.shape[2]/moving.shape[2])*1.4
-print("\nzooming...")
-moving_for_fixed = zoom(moving,(zf,yf,xf),order=0)
-
-print("\nsaving zoomed volume...")
-tif.imsave(enlargedfilename,moving_for_fixed.astype("uint16"))
-
-# copy the parameter files
-a2r = [os.path.join(transformfilepath, xx) for xx in os.listdir(transformfilepath) if "Transform" in xx]
+# where are the transform files? those are 'reg'
+reg = os.path.join(src, "output_dirs/SIGMA_to_LE_affine")
+a2r = [os.path.join(reg, xx) for xx in os.listdir(reg) if "Transform" in xx]
 a2r.sort()
+
+dst = os.path.join(src,"output_dirs/SIGMA_to_LE_transformix_affine")
+makedir(dst)
 
 transformfiles = modify_transform_files(transformfiles=a2r, dst=dst)
 [change_interpolation_order(xx, 0) for xx in transformfiles]
@@ -59,11 +45,14 @@ for fl in transformfiles:  # Read in the file
     with open(fl, "r") as file:
         filedata = file.read()
     # Replace the target string
-    filedata = filedata.replace(
-        '(ResultImagePixelType "float")', '(ResultImagePixelType "short")')
+    # filedata = filedata.replace(
+    #    '(ResultImagePixelType "float")', '(ResultImagePixelType "short")')
     # Write the file out again
     with open(fl, "w") as file:
         file.write(filedata)
 
 # run transformix
-transformix_plus_command_line_call(enlargedfilename, dst, transformfiles[-1])
+transformix_plus_command_line_call(os.path.join(
+    src, "enlarged_tiffs/SIGMA_for_LE.tif"),
+    dst, transformfiles[-1])
+
