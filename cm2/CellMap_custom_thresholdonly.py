ee117d76cfb6fb2df53ffe560c840329aab69908
Emily Dennis <emilydennis@vpn12-client-172-20-206-235.princeton.edu>
2021-03-15 11:26:22 -0400

    custom threshold updates for cluster

--- cm2/CellMap_custom_thresholdonly.py
+++ cm2/CellMap_custom_thresholdonly.py
@@ -30,32 +30,57 @@ __copyright__ = 'Copyright © 2020 by Christoph Kirst'
 __webpage__   = 'http://idisco.info'
 __download__  = 'http://www.github.com/ChristophKirst/ClearMap2'
 
+    #%% Initialize workspace
+    import os, sys
+    import tiffile as tif
+    sys.path.append("/scratch/ejdennis/rat_BrainPipe/ClearMap2")
+    from ClearMap.Environment import *  #analysis:ignore
+
+
+    def joinup_all(directory,additional_path_list,filename_to_add):
+        list_out = []
+        for newpath in additional_path_list:
+            list_out.append(os.path.join(directory,newpath,filename_to_add))
+
+    def process_list_of_files(directory, additional_path_list, filename_to_add, source_threshold, size_tuple):
+        list_of_files = joinup_all(directory,additional_path_list,filename_to_add)
+        thresholds = {
+            'source' : source_threshold,
+            'size'   : size_tuple
+        }
+    for file in list_of_files:
+        print('working on file {}'.format(file))
+        filetosave= "{}_filtered.npy".format(file[0:-8])
+        cells.filter_cells(source = file,
+                            sink = filetosave,
+                            thresholds=thresholds);
+
 if __name__ == "__main__":
 
-  #%%############################################################################
-  ### Initialization
-  ###############################################################################
+    #directories and files
+    directory = '/scratch/ejdennis'
+    smartspim_488_list = ['cm2_brains/a253/ch_488',
+                          'cm2_brains/e142/ch_488',
+                          'cm2_brains/j316/ch_488',
+                          'cm2_brains/j317/ch_488']
+    smartspim_642_list = ['cm2_brains/a253/ch_642',
+                          'cm2_brains/e142/ch_642',
+                          'cm2_brains/e143/ch_642',
+                          'cm2_brains/e144/ch_642',
+                          'cm2_brains/e153/ch_642',
+                          'cm2_brains/h234/ch_642',
+                          'cm2_brains/j316/ch_642',
+                          'cm2_brains/j317/ch_642']
+    lavision_ch00_list = ['lightsheet/z267/_ch00',
+                          'lightsheet/z265/_ch00',
+                          'lightsheet/z268/_ch00',
+                          'lightsheet/z269/_ch00']
+    filename_to_add = 'cells_raw.npy'
 
-  #%% Initialize workspace
-  import os, sys
-  import tiffile as tif
-  sys.path.append("/home/emilyjanedennis/Desktop/GitHub/ClearMap2")
-  from ClearMap.Environment import *  #analysis:ignore
 
-  #directories and files
-  directory = '/home/emilyjanedennis/Desktop/brains/'
-  listoffiles = ['/home/emilyjanedennis/Desktop/brains/z265/z265_cells_raw.npy',
-                '/home/emilyjanedennis/Desktop/brains/z266/z266_debug_cells_raw.npy',
-                '/home/emilyjanedennis/Desktop/brains/z267/debug_cells_raw.npy',
-                '/home/emilyjanedennis/Desktop/brains/z268/_ch00_cells_raw.npy',
-                '/home/emilyjanedennis/Desktop/brains/z268/_ch01_cells_raw.npy',
-                '/home/emilyjanedennis/Desktop/brains/z269/_ch01_cells_raw.npy']
-  thresholds = {
-      'source' : 3,
-      'size'   : (30,120)
-      }
-  for file in listoffiles:
-      filetosave= "{}_filtered.npy".format(file[0:-8])
-      cells.filter_cells(source = file,
-                        sink = filetosave,
-                        thresholds=thresholds);
+    process_list_of_files(directory, smartspim_488_list, filename_to_add, 3, (30,120))
+    print('done with 488')
+    process_list_of_files(directory, smartspim_642_list, filename_to_add, 3, (30,120))
+    print('done with 642')
+    process_list_of_files(directory, lavision_ch00_list, filename_to_add, 3, (30,120))
+    print('done with lavision')
