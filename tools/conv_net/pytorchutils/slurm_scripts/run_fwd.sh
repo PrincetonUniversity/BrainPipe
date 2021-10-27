12e467671b8f209c1d02860775cf74177a5c8da0
Austin Hoag <austinthomashoag@gmail.com>
2021-10-27 16:54:54 -0400

    generalize training and inference
    Former-commit-id: dd7922eef9ea918e8e69558b5abad94aeb385606

--- tools/conv_net/pytorchutils/slurm_scripts/run_fwd.sh
+++ tools/conv_net/pytorchutils/slurm_scripts/run_fwd.sh
@@ -7,10 +7,10 @@
 #SBATCH --contiguous
 #SBATCH --mem=5000 #5 gbs
 #SBATCH -t 10                # time (minutes)
-#SBATCH -o /scratch/gpfs/zmd/logs/val_%j.out
-#SBATCH -e /scratch/gpfs/zmd/logs/val_%j.err
+#SBATCH -o /tigress/ahoag/cnn/exp2/slurm_logs/cnn_inf_%j.out
+#SBATCH -e /tigress/ahoag/cnn/exp2/slurm_logs/cnn_inf_%j.err
 
-module load cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1 anaconda3/5.3.1
-. activate 3dunet
+module load cudatoolkit/10.0 cudnn/cuda-10.0/7.3.1 anaconda3/2020.11
+. activate brainpipe
 
-python run_fwd.py 20200316_peterb_zd_train models/RSUNet.py 12000 z269stackstart100 --gpus 0 --noeval --tag noeval
+python run_fwd.py exp2 /tigress/ahoag/cnn/exp2 models/RSUNet.py 12000 --gpus 0 --noeval --tag exp2
